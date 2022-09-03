9c0e98758ee42d9ff32e730df832eccc9e4a2b4a
Uvais Ahmad <uvss21@gmail.com>
2022-09-03 15:51:59 +0530

    Make structure of Alarm

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,5 @@
-console.log('this is me for Alarm Clock ');
+setInterval(()=>{
+	let time = new Date();
+	let h1 = document.querySelector('h1');
+	h1.textContent = time.getHours() +':'+time.getMinutes()+':'+time.getSeconds();
+},1000);
