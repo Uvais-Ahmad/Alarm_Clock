b50b0e62eca6caa1cf8b7efc6d66dd7a784b4e10
Uvais Ahmad <uvss21@gmail.com>
2022-09-19 00:06:57 +0530

    setting curnt time to alarm default value

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,8 @@
 let setTime = document.getElementById('time');
 let setDate = document.getElementById('date');
+let hoursDom =	document.getElementById('hours');
+let mintsDom = document.getElementById('mints');
+let  meridiemDom = document.getElementsByClassName('meridiem')[0];
 
 setInterval(()=>{
 	let time = new Date();
@@ -7,27 +10,27 @@ setInterval(()=>{
 	
 	setDate.textContent = time.toLocaleDateString('en-US', options)
 	setTime.textContent = time.toLocaleTimeString();
-	
 },1000);
 
-
-
+handleSetTime();
 function handleSetTime(){
 	let time = new Date();
 	// time.getHours() +':'+time.getMinutes()+':'+time.getSeconds();
+	// let secs = time.getSeconds();
 	let hours = time.getHours();
 	let mints = time.getMinutes();
-	let secs = time.getSeconds();
 	let meridiem = hours >= 12 ? 'PM' : 'AM' ;
 	hours = hours > 12 ? hours - 12 : hours; 
 
 	hours = appendZero(hours);
 	mints = appendZero(mints);
-	secs = appendZero(secs);
+	// secs = appendZero(secs);
+	hoursDom.value = hours;
+	mintsDom.value = mints;
+	meridiemDom.value = meridiem;
 	
-	
-	let currentTime = hours+' : '+mints+' : '+secs+' : '+meridiem;
-	return currentTime;
+	// let currentTime = hours+' : '+mints+' : '+secs+' : '+meridiem;
+	// return currentTime;
 }
 
 function appendZero(n){
